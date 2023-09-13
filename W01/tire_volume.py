ad332c15db2bd52cf1a4ad10fe44578f9b62a4cc
chronojean <megachronoje@gmail.com>
2023-09-13 15:08:08 -0400

    whitespaces and endlines

--- W01/tire_volume.py
+++ W01/tire_volume.py
@@ -52,5 +52,7 @@ clear_cls()
 w = float(input(f"Enter the width of the tire in mm (ex 205): "))
 a = float(input(f"Enter the aspect ratio of the tire (ex 60): "))
 d = float(input(f"Enter the diameter of the wheel in inches (ex 15): "))
+
 v = (math.pi*(w**2)*a*(w*a+2540*d))/10**10
+
 print(f"\nThe approximate volume is {round(v,2)} liters.\n")
