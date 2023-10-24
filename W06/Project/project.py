9a353951a5b9e3e642af4bc15b480ff7bda3e560
chronojean <megachronoje@gmail.com>
2023-10-24 19:00:16 -0400

    Summary form finished

--- W06/Project/project.py
+++ W06/Project/project.py
@@ -3,9 +3,10 @@ import calendar
 import sys
 from screeninfo import get_monitors
 from tkinter import Frame, Label, Button,ttk,Entry
-from number_entry import IntEntry
 from datetime import datetime
+from tkcalendar import DateEntry
 
+filename = "history.txt"
 palette = {
     "bg_main": "#002400",               #Main Background Color
     "bg_1": "#556B2F",                  #Background color for lesser elements
@@ -21,82 +22,54 @@ palette = {
 def main():
     ventana = tk.Tk()
     ventana.title("My finances")
-    
+    s = ttk.Style(ventana)
+    s.theme_use('clam')
+    s.configure('TCombobox', arrowsize=26)
+    s.configure('DateEntry', arrowsize=26)
     create_all_widgets(ventana)
-
+    ventana.config(background=palette["bg_main"])
     ventana.mainloop()
-
+    
 def create_all_widgets(ventana):
     #form
     create_section0(ventana)
     #grid
     create_section1(ventana)
+    #summary
+    create_section2(ventana)
+    ventana.update_idletasks()
     
 def create_section0(ventana):
     section0 = Frame(ventana,background=palette["bg_main"],relief="flat",borderwidth=0,name="section0")
-    section0.pack(padx=0, pady=0, fill=tk.BOTH, expand=1,side="top",anchor="center")
+    section0.pack()
     create_form(ventana)
 
 def create_form(ventana):
-    section0 = ventana.children.get("section0")
-    
+    section0 = ventana.children.get("section0")    
     lbl_amount = Label(section0, text="Amount: ",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"])
-    entry_amount = Entry(section0,width=12,name="entry_amount")    
+    entry_amount = Entry(section0,width=12,name="entry_amount",justify="right")
     lbl_of = Label(section0, text=" of ",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_main"])
-    income_outcome = ttk.Combobox(section0, values=["Income", "Outcome"],font=palette["font_1"],state="readonly")
+    income_outcome = ttk.Combobox(section0, values=["Income", "Outcome"],font=palette["font_1"],state="readonly",name="income_outcome")
     income_outcome["width"] = max(len(option) for option in income_outcome["values"])+2
     income_outcome.set("Income")
-    btn_save_income_outcome = Button(section0, text="Save",relief="solid",background=palette["bg_2"],fg=palette["font_color_main"],font=palette["font_1"],command=lambda: save_income_outcome(ventana,entry_amount.get(),income_outcome.get()))
-    
-    lbl_amount.pack(side=tk.LEFT)
-    entry_amount.pack(side=tk.LEFT)
-    lbl_of.pack(side=tk.LEFT)
-    income_outcome.pack(side=tk.LEFT)
-    btn_save_income_outcome.pack(side=tk.LEFT)
-
-def save_income_outcome(ventana,amount,income_outcome):
-    try:
-        monto=round(float(amount),0)
-        str = f"{datetime.now(tz='America/Caracas')}"
-        create_modal_window(f"{income_outcome} saved.")
-        try:
-            ventana.children.get("section0").children.get("entry_amount").delete(0,tk.END)
-            reset_grid(ventana)
-        except:
-            None
-    except:
-        create_modal_window("Please enter a valid number. Use a period (.) as the thousands separator and a comma (,) as the decimal separator.")   
-
-def create_modal_window(message):
-    modal_window = tk.Toplevel()
-    modal_window.overrideredirect(True)
-    label = tk.Label(
-        modal_window,
-        text=message,
-        background="#337a2c",
-        fg="white",
-        font=("Calibri", 16, "bold"),
-        justify="left",
-        wraplength=800,
-        padx=10,
-        pady=5,
-        relief="solid",
-        border=3
-    )
-    label.pack()
-    def close_modal_window(event):
-        modal_window.destroy()
-    modal_window.bind("<Button-1>", close_modal_window)
-    #print(f'{label.winfo_reqwidth()}x{label.winfo_reqheight()}')
-    resolution= obtener_resolucion_monitor_actual()
-    #print(resolution)
-    modal_window.geometry(f'{label.winfo_reqwidth()}x{label.winfo_reqheight()}+{resolution[0]-label.winfo_reqwidth()}+{resolution[1]-label.winfo_reqheight()}')
-    modal_window.after(5000, lambda: close_modal_window(None))
+    cal = DateEntry(section0, width=12, background='darkblue',foreground='white', borderwidth=2, state="readonly",name="date_transaction")
+    lbl_desc = Label(section0,text="Description",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"])
+    entry_desc = Entry(section0,name="entry_desc", justify="left")
+    btn_save_income_outcome = Button(section0, text="Save",relief="solid",background=palette["bg_2"],fg=palette["font_color_main"],font=palette["font_1"],command=lambda: save_income_outcome(ventana,entry_amount.get(),income_outcome.get(),entry_desc.get(),cal.get_date()))
+    lbl_amount.grid(row=0,column=0,sticky="nsew")
+    entry_amount.grid(row=0,column=1,sticky="nsew")
+    lbl_of.grid(row=0,column=2,sticky="nsew")
+    income_outcome.grid(row=0,column=3,sticky="nsew")
+    cal.grid(row=0,column=4,sticky="nsew")
+    #Second Row
+    lbl_desc.grid(row=1,column=0,sticky="nsew")
+    entry_desc.grid(row=1,column=1,columnspan=3,sticky="nsew")
+    btn_save_income_outcome.grid(row=1,column=4)
 
 def create_section1(ventana):
     section1 = Frame(ventana, background=palette["bg_main"], relief="flat", borderwidth=0,name="section1")
     section1.pack(padx=0, pady=0, fill=tk.BOTH, expand=1)
-    create_grid(ventana,load_history("history.txt"))
+    create_grid(ventana,load_history(filename))
 
 def create_grid(ventana,data):
     frame = ventana.children.get("section1")
@@ -116,18 +89,83 @@ def create_grid(ventana,data):
             else:
                 label = Label(frame, text=date_short_day(item) if j==0 else item, anchor="w",justify="left",background=palette["bg_1"],font=palette["font_main"],fg=palette["font_color_main"])
                 label.grid(row=i, column=j, padx=palette["pad_1"], pady=0, sticky="nsew")
-    label = Label(frame,text="Current Money: ",background=palette["bg_main"],fg="White",relief="sunken",font=palette["font_2"])
-    label.grid(row=i+1,columnspan=2,sticky="nsew")
-    label = Label(frame,text=f'{sum([row[2] for row in data]):,}',background=palette["bg_2"],fg="White",relief="sunken",font=palette["font_2"])
-    label.grid(row=i+1,column=2,columnspan=2,sticky="nsew")
-    for i in range(len(data)):
-        frame.grid_rowconfigure(i, weight=1)
-    for j in range(len(data[0])):
-        frame.grid_columnconfigure(j, weight=1)
+    try:
+        label = Label(frame,text="Current Money: ",background=palette["bg_main"],fg="White",relief="sunken",font=palette["font_2"])
+        label.grid(row=i+1,columnspan=2,sticky="nsew")
+        label = Label(frame,text=f'{sum([row[2] for row in data]):,}',background=palette["bg_2"],fg="White",relief="sunken",font=palette["font_2"])
+        label.grid(row=i+1,column=2,columnspan=2,sticky="nsew")
+        for i in range(len(data)):
+            frame.grid_rowconfigure(i, weight=1)
+        for j in range(len(data[0])):
+            frame.grid_columnconfigure(j, weight=1)
+    except:
+        None
 
 def reset_grid(ventana):
     ventana.children.get("section1").destroy()
     create_section1(ventana)
+    
+def create_section2(ventana):
+    section2 = Frame(ventana,background=palette["bg_main"],relief="flat",borderwidth=0,name="section2")
+    create_summary(ventana)
+    section2.pack(padx=0, pady=0, fill=tk.BOTH, expand=1)
+
+def create_summary(ventana):
+    section2 = ventana.children.get("section2")
+    section2.grid_rowconfigure(0, weight=1)
+    section2.grid_columnconfigure((0, 1, 2, 3), weight=1)
+    
+    lbl_summary_title_type = Label(section2, text="              ",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"])
+    lbl_summary_title_income = Label(section2, text="Income",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"])
+    lbl_summary_title_outcome = Label(section2, text="Outcome",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"])
+    lbl_summary_title_balance = Label(section2, text="Balance",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"])
+    
+    lbl_summary_daily_type = Label(section2, text="daily summary",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"],anchor="w", justify="left")
+    lbl_summary_daily_income = Label(section2, text="daily summary Income",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    lbl_summary_daily_outcome = Label(section2, text="daily summary Outcome",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    lbl_summary_daily_balance = Label(section2, text="daily summary Balance",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    
+    lbl_summary_weekly_type = Label(section2, text="weekly summary",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"],anchor="w", justify="left")
+    lbl_summary_weekly_income = Label(section2, text="weekly summary Income",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    lbl_summary_weekly_outcome = Label(section2, text="weekly summary Outcome",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    lbl_summary_weekly_balance = Label(section2, text="weekly summary Balance",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    
+    lbl_summary_monthly_type = Label(section2, text="Monthly summary",background=palette["bg_main"],fg=palette["font_color_main"],font=palette["font_1"],anchor="w", justify="left")
+    lbl_summary_monthly_income = Label(section2, text="Monthly summary Income",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    lbl_summary_monthly_outcome = Label(section2, text="Monthly summary Outcome",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    lbl_summary_monthly_balance = Label(section2, text="Monthly summary Balance",background=palette["bg_1"],fg=palette["font_color_main"],font=palette["font_1"],anchor="e", justify="right")
+    
+    lbl_summary_title_type.grid(row=0,column=0,sticky="nsew",padx=1,pady=1)
+    lbl_summary_title_income.grid(row=0,column=1,sticky="nsew",padx=1,pady=1)
+    lbl_summary_title_outcome.grid(row=0,column=2,sticky="nsew",padx=1,pady=1)
+    lbl_summary_title_balance.grid(row=0,column=3,sticky="nsew",padx=1,pady=1)
+    
+    lbl_summary_daily_type.grid(row=1,column=0,sticky="nsew",padx=1)
+    lbl_summary_daily_income.grid(row=1,column=1,sticky="nsew",padx=1)
+    lbl_summary_daily_outcome.grid(row=1,column=2,sticky="nsew",padx=1)
+    lbl_summary_daily_balance.grid(row=1,column=3,sticky="nsew",padx=1)
+    
+    lbl_summary_weekly_type.grid(row=2,column=0,sticky="nsew",padx=1)
+    lbl_summary_weekly_income.grid(row=2,column=1,sticky="nsew",padx=1)
+    lbl_summary_weekly_outcome.grid(row=2,column=2,sticky="nsew",padx=1)
+    lbl_summary_weekly_balance.grid(row=2,column=3,sticky="nsew",padx=1)
+    
+    lbl_summary_monthly_type.grid(row=3,column=0,sticky="nsew",padx=1)
+    lbl_summary_monthly_income.grid(row=3,column=1,sticky="nsew",padx=1)
+    lbl_summary_monthly_outcome.grid(row=3,column=2,sticky="nsew",padx=1)
+    lbl_summary_monthly_balance.grid(row=3,column=3,sticky="nsew",padx=1)
+   
+def create_modal_window(message):
+    modal_window = tk.Toplevel()
+    modal_window.overrideredirect(True)
+    label = tk.Label(modal_window,text=message,background="#337a2c",fg="white",font=("Calibri", 16, "bold"),justify="left",wraplength=800,padx=10,pady=5,relief="solid",border=3)
+    label.pack()
+    def close_modal_window(event):
+        modal_window.destroy()
+    modal_window.bind("<Button-1>", close_modal_window)
+    resolution= obtener_resolucion_monitor_actual()
+    modal_window.geometry(f'{label.winfo_reqwidth()}x{label.winfo_reqheight()}+{resolution[0]-label.winfo_reqwidth()}+{resolution[1]-label.winfo_reqheight()}')
+    modal_window.after(5000, lambda: close_modal_window(None))
 
 def load_history(filename):
     try:
@@ -143,8 +181,8 @@ def load_history(filename):
                 arr.append(row)
         return arr
     except (FileNotFoundError,PermissionError):
-        #print("Couldn't open the history file")
-        sys.exit(0)
+        create_modal_window("Couldn't open the history file")
+        return []
          
 def es_numero(valor):
     try:
@@ -152,6 +190,7 @@ def es_numero(valor):
         return True
     except ValueError:
         return False
+    
 def date_short_day(given_date):
     # Convertir la fecha del archivo a un objeto datetime
     fecha_objeto = datetime.strptime(given_date, "%Y/%m/%d")
@@ -164,8 +203,26 @@ def date_short_day(given_date):
 
     return fecha_formateada
 
+def save_income_outcome(ventana,amount,income_outcome,description,date_of_data):
+    if validate_data(ventana,amount,income_outcome,description,date_of_data):
+        append_to_file(f"{date_of_data.strftime('%Y/%m/%d')}, {description}, {float(amount) if income_outcome=='Income' else (float(amount)*-1)}")
+        reset_grid(ventana)
+
+        create_modal_window("Entry saved.")
+
+def validate_data(ventana,amount,income_outcome,description,date_of_data):
+    if not es_numero(amount) or float(amount) <=0:
+        create_modal_window(f"The amount must be a positive number.")
+        return False
+    elif date_of_data>datetime.now().date():
+        create_modal_window(f"The selected date is in the future. Please enter a valid date.")
+        return False
+    elif len(description)==0:
+        create_modal_window(f"Please enter a description for this transaction.")
+        return False
+    return True
+
 def obtener_resolucion_monitor_actual():
-    # Obtiene una lista de todos los monitores conectados
     monitores = get_monitors()
 
     if monitores:
@@ -175,9 +232,17 @@ def obtener_resolucion_monitor_actual():
         alto = monitor_principal.height
         return [ancho,alto]
     else:
-        return "No se encontraron monitores."   
+        return "No se encontraron monitores."
 
-
+def append_to_file(linea):
+    try:
+        with open(filename, "a+") as file:
+            file.write(f"{linea}\n")
+            file.seek(0)
+            content = file.read()
+            return content
+    except IOError:
+        input("An error occurred while opening the file. Maybe the file is blocked or marked as 'read only' or you don't have the permission to access the file")
 
 if __name__ == "__main__":
     main()
--- W06/Project/test.py
+++ W06/Project/test.py
@@ -1,38 +1,33 @@
-try:
-    import tkinter as tk
-    from tkinter import ttk
-except ImportError:
-    import Tkinter as tk
-    import ttk
+import tkinter as tk
+from tkinter import ttk
 
-from tkcalendar import Calendar, DateEntry
+def center_window(window):
+    window.update_idletasks()
+    width = window.winfo_width()
+    height = window.winfo_height()
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
-def example1():
-    def print_sel():
-        print(cal.selection_get())
+def create_all_widgets(ventana):
+    # Aquí debes agregar el código para crear todos los widgets de tu ventana
+    pass
 
-    top = tk.Toplevel(root)
+def main():
+    ventana = tk.Tk()
+    ventana.title("My finances")
+    s = ttk.Style(ventana)
+    s.theme_use('clam')
+    s.configure('TCombobox', arrowsize=26)
+    s.configure('DateEntry', arrowsize=26)
+    
+    create_all_widgets(ventana)
 
-    cal = Calendar(top,
-                   font="Arial 14", selectmode='day',
-                   cursor="hand1", year=2018, month=2, day=5)
-    cal.pack(fill="both", expand=True)
-    ttk.Button(top, text="ok", command=print_sel).pack()
+    # Centrar la ventana en la pantalla
+    center_window(ventana)
 
-def example2():
-    top = tk.Toplevel(root)
+    ventana.mainloop()
 
-    ttk.Label(top, text='Choose date').pack(padx=10, pady=10)
-
-    cal = DateEntry(top, width=12, background='darkblue',
-                    foreground='white', borderwidth=2,state="readonly")
-    cal.pack(padx=10, pady=10)
-
-root = tk.Tk()
-s = ttk.Style(root)
-s.theme_use('clam')
-
-ttk.Button(root, text='Calendar', command=example1).pack(padx=10, pady=10)
-ttk.Button(root, text='DateEntry', command=example2).pack(padx=10, pady=10)
-
-root.mainloop()
+main()
